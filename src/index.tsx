c450c73a01a7f496840cb21a16fef3be6458a26e
Anar Sadigli <anar.sadigli@coolbet.com>
2022-12-10 21:38:20 +0200

    PLANLA-7 files moved to src folder

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>asdas App</h1>
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
